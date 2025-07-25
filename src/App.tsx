ed4730c434309bf910c6b42ead2a5d037636628b
Abdul Rehman Nasir Tobaria <116266103+abdulrehmann231@users.noreply.github.com>
2025-07-25 09:00:31 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="bg-gray-100 min-h-screen flex items-center justify-center">
+    <main className="bg-gray-200 min-h-screen flex items-center justify-center">
       <div className="bg-white p-8 rounded shadow text-center">
         <h1 className="text-2xl font-bold mb-4">
           Welcome to my ⚛️ React app built with ⚡ Vite!
